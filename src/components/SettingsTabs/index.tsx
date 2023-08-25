c01d329406cedda96e9b8c5489976b94f38d2537
João Gabriel <jgomachado@hotmail.com>
2023-08-25 16:05:19 -0300

    responsive ok

--- src/components/SettingsTabs/index.tsx
+++ src/components/SettingsTabs/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import * as Tabs from '@radix-ui/react-tabs'
+import * as ScrollArea from '@radix-ui/react-scroll-area'
 import { TabItem } from './TabItem'
 import { useState } from 'react'
 
@@ -9,46 +10,69 @@ export function SettingsTabs() {
 
   return (
     <Tabs.Root value={currentTab} onValueChange={setCurrentTab}>
-      <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-200">
-        <TabItem
-          value="my-details"
-          title="My details"
-          isSelected={currentTab === 'my-details'}
-        />
-        <TabItem
-          value="Profile"
-          title="Profile"
-          isSelected={currentTab === 'Profile'}
-        />
-        <TabItem
-          value="Password"
-          title="Password"
-          isSelected={currentTab === 'Password'}
-        />
-        <TabItem value="Team" title="Team" isSelected={currentTab === 'Team'} />
-        <TabItem value="Plan" title="Plan" isSelected={currentTab === 'Plan'} />
-        <TabItem
-          value="Billing"
-          title="Billing"
-          isSelected={currentTab === 'Billing'}
-        />
-        <TabItem
-          value="Email"
-          title="Email"
-          isSelected={currentTab === 'Email'}
-        />
-        <TabItem
-          value="Notifications"
-          title="Notifications"
-          isSelected={currentTab === 'Notifications'}
-        />
-        <TabItem
-          value="Integrations"
-          title="Integrations"
-          isSelected={currentTab === 'Integrations'}
-        />
-        <TabItem value="API" title="API" isSelected={currentTab === 'API'} />
-      </Tabs.List>
+      <ScrollArea.Root className="w-full" type="scroll">
+        <ScrollArea.Viewport className="w-full overflow-x-scroll">
+          <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-200">
+            <TabItem
+              value="my-details"
+              title="My details"
+              isSelected={currentTab === 'my-details'}
+            />
+            <TabItem
+              value="Profile"
+              title="Profile"
+              isSelected={currentTab === 'Profile'}
+            />
+            <TabItem
+              value="Password"
+              title="Password"
+              isSelected={currentTab === 'Password'}
+            />
+            <TabItem
+              value="Team"
+              title="Team"
+              isSelected={currentTab === 'Team'}
+            />
+            <TabItem
+              value="Plan"
+              title="Plan"
+              isSelected={currentTab === 'Plan'}
+            />
+            <TabItem
+              value="Billing"
+              title="Billing"
+              isSelected={currentTab === 'Billing'}
+            />
+            <TabItem
+              value="Email"
+              title="Email"
+              isSelected={currentTab === 'Email'}
+            />
+            <TabItem
+              value="Notifications"
+              title="Notifications"
+              isSelected={currentTab === 'Notifications'}
+            />
+            <TabItem
+              value="Integrations"
+              title="Integrations"
+              isSelected={currentTab === 'Integrations'}
+            />
+            <TabItem
+              value="API"
+              title="API"
+              isSelected={currentTab === 'API'}
+            />
+          </Tabs.List>
+        </ScrollArea.Viewport>
+
+        <ScrollArea.Scrollbar
+          className="flex h-0.5 translate-y-1.5 touch-none select-none flex-col bg-zinc-100"
+          orientation="horizontal"
+        >
+          <ScrollArea.Thumb className="relative flex-1 rounded-lg bg-zinc-300" />
+        </ScrollArea.Scrollbar>
+      </ScrollArea.Root>
     </Tabs.Root>
   )
 }
